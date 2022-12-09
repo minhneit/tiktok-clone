0c050b52aa2a7ebb60baaed32d4a27b1387d5a83
minhenit <minhneit.wingroup@gmail.com>
2022-12-09 09:29:30 +0700

    add commit

---
+++ src/pages/Upload/Upload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Layout/DefaultLayout/Hedaer';
+function Upload() {
+    return (
+        <div>
+            <Header />
+            <h1>Upload</h1>
+        </div>
+    );
+}
+
+export default Upload;
